c9c0785a2eeefd855025ba6ef641d458dff13766
Lei Wang <lwang025@coupang.com>
2021-11-05 14:15:23 -0700

    minor changes

--- sdk/src/trace/samplers/trace_id_ratio.cc
+++ sdk/src/trace/samplers/trace_id_ratio.cc
@@ -125,11 +125,14 @@ double getSamplingRate(std::string cmdb, std::string env){
       std::cout<< cmdb <<" cmdb in sample rate cc.\n";
       std::cout<< env <<" env in sample rate cc.\n";
       lastUpdatedTime = cur_timestamp;
+      std::string cur_token = env=="prod"?"4e13740e-9d65-39eb-e0c3-473397658ea6":"eb438d90-4183-06d7-0095-8e24d723c9c6";
+      std::string cur_url = env == "prod"?"http://internal-ms-service-discovery-887102973.ap-northeast-2.elb.amazonaws.com/":"http://10.213.211.43:8500";
+      td::cout<< cur_token <<" cur_token in sample rate cc.\n";
+      std::cout<< cur_url <<" cur_url in sample rate cc.\n";
+      ppconsul::Consul consul(cur_url,kw::token=cur_token);
+      Kv kv(consul,kw::token=cur_token);
       
-      ppconsul::Consul consul(env == "prod"?"http://internal-ms-service-discovery-887102973.ap-northeast-2.elb.amazonaws.com/":"http://10.213.211.43:8500",kw::token="eb438d90-4183-06d7-0095-8e24d723c9c6");
-      Kv kv(consul,kw::token=env=="prod"?"4e13740e-9d65-39eb-e0c3-473397658ea6":"eb438d90-4183-06d7-0095-8e24d723c9c6");
-      
-      return stod(kv.get("hot_config/coutrace/nginx/" +  cmdb, "0.01", kw::token=env=="prod"?"4e13740e-9d65-39eb-e0c3-473397658ea6":"eb438d90-4183-06d7-0095-8e24d723c9c6"));
+      return stod(kv.get("hot_config/coutrace/nginx/" +  cmdb, "0.01", kw::token=cur_token));
     }
     return -1.0;
     //}00
